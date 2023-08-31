dcd1a273d8c28c14684091b611bcd394d6b3922c
Mukul Mantosh <mukulmantosh@yahoo.com>
2023-08-31 11:44:31 +0530

    WIP db,cart

--- controllers/cart.go
+++ controllers/cart.go
@@ -1,9 +1,58 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"ecommerce-gin/database"
+	"errors"
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"log"
+	"net/http"
+	"time"
+)
 
-func AddToCart() gin.HandlerFunc {
+type Application struct {
+	prodCollection *mongo.Collection
+	userCollection *mongo.Collection
+}
 
+func NewApplication(prodCollection, userCollection *mongo.Collection) *Application {
+	return &Application{prodCollection: prodCollection, userCollection: userCollection}
+}
+
+func (app *Application) AddToCart() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		productQueryID := c.Query("id")
+		if productQueryID == "" {
+			log.Println("Product ID is empty")
+			_ = c.AbortWithError(http.StatusBadRequest, errors.New("product id is empty"))
+
+			userQueryID := c.Query("userID")
+			if userQueryID == "" {
+				log.Println("user id is empty")
+				c.AbortWithError(http.StatusBadRequest, errors.New("user id is empty"))
+				return
+			}
+
+			productID, err := primitive.ObjectIDFromHex(productQueryID)
+
+			if err != nil {
+				log.Println(err)
+				c.AbortWithStatus(http.StatusInternalServerError)
+				return
+			}
+
+			var ctx, cancel = context.WithTimeout(context.Background(), 5*time.Second)
+			defer cancel()
+
+			database.AddProductToCart(ctx, app.prodCollection, app.userCollection, productID, userQueryID)
+			if err != nil {
+				c.IndentedJSON(http.StatusInternalServerError, err)
+			}
+			c.IndentedJSON(http.StatusOK, "successfully added")
+		}
+	}
 }
 
 func RemoveItem() gin.HandlerFunc {
--- controllers/controllers.go
+++ controllers/controllers.go
@@ -1,6 +1,16 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import "C"
+import (
+	"context"
+	"ecommerce-gin/models"
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"log"
+	"net/http"
+	"time"
+)
 
 func HashPassword(password string) string {
 
@@ -11,11 +21,101 @@ func VerifyPassword(userPassword string, givenPassword string) (bool, string) {
 }
 
 func Signup() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
 
+		var user models.User
+		if err := c.Bind(&user); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		validationErr := Validate.Struct(user)
+		if validationErr != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": validationErr})
+		}
+
+		count, err := UserCollection.CountDocuments(ctx, bson.M{"email": user.Email})
+		if err != nil {
+			log.Panic(err)
+			return
+		}
+
+		if count > 0 {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "user already exists!"})
+		}
+
+		count, err = UserCollection.CountDocuments(ctx, bson.M{"phone": user.Phone})
+
+		if err != nil {
+			log.Panic(err)
+			return
+		}
+
+		if count > 0 {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "this phone number is already in use"})
+			return
+		}
+
+		password := HashPassword(user.Password)
+		user.Password = password
+		user.CreatedAt, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		user.UpdatedAt, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		user.ID = primitive.NewObjectID()
+		user.UserID = user.ID.Hex()
+
+		token, refreshToken, _ := generate.TokenGenerator(user.Email, user.FirstName, user.LastName, user.UserID)
+
+		user.Token = &token
+		user.RefreshToken = &refreshToken
+		user.UserCart = make([]models.ProductUser, 0)
+		user.AddressDetails = make([]models.Address, 0)
+		user.OrderStatus = make([]models.Order, 0)
+		_, insertErr := UserCollection.InsertOne(ctx, user)
+		if insertErr != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Unable to create user"})
+			return
+		}
+
+		c.JSON(http.StatusCreated, "Successfully signed in!")
+
+	}
 }
 
 func Login() gin.HandlerFunc {
+	return func(c *gin.Context) {
 
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+
+		var user models.User
+		if err := c.BindJSON(&user); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err})
+			return
+		}
+		err := UserCollection.FindOne(ctx, bson.M{"email": user.Email}).Decode(&foundUser)
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "login or password incorrect"})
+			return
+		}
+
+		PasswordIsValid, msg := VerifyPassword(user.Password, foundUser.Password)
+
+		if !PasswordIsValid {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
+			log.Println(msg)
+			return
+		}
+		token, refreshToken, _ := generate.TokenGenerator(foundUser.Email, foundUser.FirstName,
+			foundUser.LastName, foundUser.UserID)
+
+		generate.UpdateAllTokens(token, refreshToken, foundUser.UserID)
+
+		c.JSON(http.StatusFound, foundUser)
+		return
+
+	}
 }
 
 func ProductViewerAdmin() gin.HandlerFunc {
